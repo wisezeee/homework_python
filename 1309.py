3a14fc2e87e108bf5b528b5d26061aa4b715f968
wisezeee <113104715+wisezeee@users.noreply.github.com>
2022-09-12 19:08:14 +0300

    1309

---
+++ 1309.py
@@ -0,0 +1,24 @@
+def count(l):
+    small = 0
+    large = 0
+    for i in l:
+        if i.islower():
+            small += 1
+        elif i.isupper():
+            large += 1
+    if large > small:
+        return 1
+    elif large < small:
+        return -1
+
+def allStr(mas):
+    counter = 0
+    for i in (mas):
+        y = count(i)
+        if y == 1:
+            counter += 1
+    print('Заглавных букв больше у ' + str(counter / len(mas) * 100) + '% строк')
+
+x = str(input())
+mas = x.split()
+allStr(mas)
