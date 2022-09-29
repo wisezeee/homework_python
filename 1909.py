a6a0c3e882c4756bc33cf384cd79b8376d6e897e
wisezeee <113104715+wisezeee@users.noreply.github.com>
2022-09-29 09:24:38 +0300

    Create 1909.py

---
+++ 1909.py
@@ -0,0 +1,14 @@
+def cal(d, m, y):
+    m_30d = [4, 6, 9, 11]
+    m_31d = [1, 3, 5, 7, 8, 10, 12]
+    if 0 < d <= 31 and m in m_31d:
+        return True
+    if 0 < d <= 30 and m in m_30d:
+        return True
+    if m == 2:
+        if y % 100 != 0 and y % 4 == 0 or y % 100 == 0 and y % 400 == 0:   #Високосный год кратен 4, но при этом не кратен 100, либо кратен 400
+            if 0 < d <= 29:
+                return True
+        if 0 < d <= 28:
+            return True
+    return False
