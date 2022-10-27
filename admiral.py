b45c0c340c3e70de60450facd3c1f324a05cb71e
wisezeee <113104715+wisezeee@users.noreply.github.com>
2022-10-27 11:39:33 +0300

    Create admiral.py

---
+++ admiral.py
@@ -0,0 +1,16 @@
+import os.path
+
+
+def admiral(filename):
+    path = os.path.join(os.path.dirname(__file__))
+    if filename in os.listdir(path):
+        msg = input('[+] Запись капитана: ')
+        time = input('[+] Дата и время: ')
+        with open(f'{path}/{filename}', 'a') as file:
+            file.write(f'{time} - {msg} \n')
+            file.close()
+    else:
+        with open(f'{path}/{filename}', 'w') as file:
+            file.close()
+        return admiral(filename)
+
